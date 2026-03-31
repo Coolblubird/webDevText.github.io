cd7a02d0176f072ddabd59d73df4299e9ac93941
Coolblubird <80004413+Coolblubird@users.noreply.github.com>
2026-03-31 21:10:49 +0000

    Update 7 - Hamburger menu and uiux cleanup

---
+++ SCRIPTS/script.js
@@ -0,0 +1,10 @@
+
+// Display the dropdown menu when the user clicks on the hamburger icon
+function dropMenu() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
