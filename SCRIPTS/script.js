10412f5611c00db91ce85cb7c09e98ffe90e4c81
BentleyHerbstritt <bentleyherbstritt@suumail.net>
2026-04-13 18:28:31 -0600

    story drop down Attempt 1

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -8,3 +8,25 @@ function dropMenu() {
     x.style.display = "block";
   }
 } 
+
+//Display the story when the user clicks the down icon for each of the stories
+
+function dropStoryOne() {
+  var x = document.getElementById("storyDetailOne");
+  if (x.style.display === "block") {
+    x.animate([
+      { transform: 'translateX(0px)' },
+      { transform: 'translateX(-300px)'}
+    ], {
+      duration:1000});
+    x.style.display = "none";
+    
+  } else {
+    x.style.display = "block";
+    x.animate([
+      { transform: 'translateX(-300px)' },
+      { transform: 'translateX(0px)'}
+    ], {
+      duration:1000});
+  }
+}
